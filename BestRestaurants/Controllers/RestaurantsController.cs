bb9bb587ffa0c2bc8182eb1ff40b948ed566d003
Jose Amesquita <joseamesquita11@gmail.com>
2020-03-18 15:01:56 -0700

    modify cuisine and restaurant controllers
    Co-authored-by: Matt <taylor.matt@protonmail.com>
    Co-authored-by: Dom <ddyoumans@icloud.com>

--- BestRestaurants/Controllers/RestaurantsController.cs
+++ BestRestaurants/Controllers/RestaurantsController.cs
@@ -7,8 +7,69 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BestRestaurants.Controllers
 {
-  public class CuisinesCongtroller : Controllers
+  public class RestaurantsController : Controllers
   {
+    private readonly BestRestaurantsContext _db;
 
+    public RestaurantsController(BestRestaurantsContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Restaurant> model = _db.Restaurants.Include(restaurants => restaurants.Cuisine).ToList();
+      return View(model);
+    }
+
+    [HttpPost]
+    public ActionResult Create(Restaurant restaurant)
+    {
+      _db.Restaurants.Add(restaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Create()
+    {
+      // "Name" needs to match the db
+      ViewBag.CuisineId = new SelectList(_db.Cuisines, "CuisineId", "Name");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Restaurant thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.restaurantId == id);
+      return View(thisRestaurant);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      ViewBag.CusineId = new SelectList(_db.Cuisines, "CusineId", "Name");
+      return View(thisRestaurant);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Restaurant restaurant)
+    {
+      _db.Entry(restaurant).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      return View(thisRestaurant);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisRestaurant = _db.Restaurants.FirstOrDefault(restaurants => restaurants.RestaurantId == id);
+      _db.Restaurants.Remove(thisRestaurant);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -4,7 +4,13 @@ namespace BestRestaurants.Models
 {
   public class Restaurant
   {
-
+    public int RestaurantId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string Rating { get; set; }
+    public string Review { get; set; }
+    public int CusineId { get; set; }
+    public virtual Cuisine Cuisine { get; set; }
   }
 
 }
