c7d71db403c92d9dfbf73030aef11b1a7724f48f
mtaylorpdx <taylor.matt@protonmail.com>
2020-03-19 11:46:57 -0700

    convert rating property from string to int, style
    Co-authored-by: Jose <joseamesquita11@gmail.com>
    Co-authored-by: Dom <ddyoumans@icloud.com>

--- BestRestaurants/Models/Restaurant.cs
+++ BestRestaurants/Models/Restaurant.cs
@@ -7,7 +7,7 @@ namespace BestRestaurants.Models
     public int RestaurantId { get; set; }
     public string Name { get; set; }
     public string Location { get; set; }
-    public string Rating { get; set; }
+    public int Rating { get; set; }
     public string Review { get; set; }
     public int CuisineId { get; set; }
     public virtual Cuisine Cuisine { get; set; }
